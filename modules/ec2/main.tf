45a98505ace9d9b68296c29355538c74fb6afad4
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-06-16 23:08:58 +0900

    보안그룹 추가

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -21,3 +21,15 @@ data "aws_ami" "amazon_linux_2_kernel_5" {
 output "linux_ami_id" {
   value = data.aws_ami.amazon_linux_2_kernel_5.id
 }
+
+resource "aws_security_group" "aws_linux_sg" {
+  name   = "linux-sg"
+  vpc_id = data.aws_vpc.default.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+}
