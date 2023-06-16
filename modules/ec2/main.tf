d40824e6560de1bf35a5cd69b8bb6ab012611853
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-06-16 22:47:08 +0900

    vpc, subnet, ami data 추가

---
+++ modules/ec2/main.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "amazon-linux-2-kernel-5" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5*"]
+  }
+}
+
+output "ami-id" {
+  value = data.aws_ami.amazon-linux-2-kernel-5.id
+}
