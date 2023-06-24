44f6a9251b8f860f4a2c5b3575cc21111cc14fdb
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-06-24 14:41:27 +0900

    ec2 인스턴스 생성

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -33,3 +33,26 @@ resource "aws_security_group" "aws_linux_sg" {
     description = "Allow incoming HTTP connections"
   }
 }
+
+resource "aws_instance" "linux_server" {
+  ami                    = data.aws_ami.amazon_linux_2_kernel_5.id
+  instance_type          = var.linux_instance_type
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.aws_linux_sg.id]
+  user_data              = file("aws-user-data.sh")
+
+  root_block_device {
+    volume_size           = 8
+    volume_type           = var.linux_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
+  }
+}
+
+output "linux_server_public_dns" {
+  value = aws_instance.linux_server.public_dns
+}
+
+output "linux_server_public_ip" {
+  value = aws_instance.linux_server.public_ip
+}
