64dc898d6d624fddc72ee6809f78908426fbec00
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-06-24 18:07:09 +0900

    user-data 인스턴스 가용영역 가져오도록 수정

--- modules/ec2/aws-user-data.sh
+++ modules/ec2/aws-user-data.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 168.126.63.1" >> /etc/resolv.conf
+echo "nameserver 210.220.163.82" >> /etc/resolv.conf
+echo "nameserver 164.124.101.2" >> /etc/resolv.conf
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+availability_zone=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "Availability Zone : <span style='color:blue;'>$(echo $availability_zone)</span>" > /var/www/html/index.html
