84efb6097350591ae5f9e9cde2cf95668d336f27
juyeonkim97 <lhkl@naver.com>
2023-07-15 17:30:22 +0900

    user data 재실행 될 수 있도록 수정

--- modules/ec2/instance-type-user-data.sh
+++ modules/ec2/instance-type-user-data.sh
@@ -1,3 +1,22 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//                                      
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//                                                                
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
@@ -10,3 +29,4 @@ sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 instance_type=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 echo "EC2 Instance Type : <span style='color:blue;'>$(echo $instance_type)</span>" > /var/www/html/index.html
+--//--  
